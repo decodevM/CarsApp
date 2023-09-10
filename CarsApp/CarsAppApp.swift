b8f7fa786e3551fc9c55eb1b696f65467a95e7e9
devodevM <decodev.contact@gmail.com>
2023-09-10 10:20:12 +0100

    Initial Commit

---
+++ CarsApp/CarsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsAppApp.swift
+//  CarsApp
+//
+//  Created by Yacine on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CarsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
